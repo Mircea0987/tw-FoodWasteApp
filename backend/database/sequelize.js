cdbe6e1f73a1fc95d22cbbafc6c0d69893dd6a18
mircostin <fedemir04@gmail.com>
2025-12-24 23:38:21 +0200

    .

---
+++ backend/database/sequelize.js
@@ -0,0 +1,21 @@
+const { Sequelize } = require("sequelize");
+
+const sequelize = new Sequelize(
+    process.env.USER_AND_NAME,
+    process.env.USER_AND_NAME,          
+    process.env.PASS,   
+    {
+        host: process.env.HOST, 
+        dialect: "postgres",
+        port: 5432,
+        dialectOptions: {
+            ssl: {
+                require: true, 
+                rejectUnauthorized: false 
+            }
+        },
+        logging: false 
+    }
+);
+
+module.exports = sequelize;
